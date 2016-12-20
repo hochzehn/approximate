5ed8696fb1f2750533b8e0239010f778be78857a
janpapenbrock <j.papenbrock@hochzehn.com>
2016-12-20 15:41:32 +0100

    Refactor tests to be more concise.

--- test/spec/approximateSpec.js
+++ test/spec/approximateSpec.js
@@ -1,66 +1,23 @@
 describe("approximate", function () {
 
+    var power2 = function(input) {
+        return Math.pow(2, input);
+    };
+
     it("should find correct input value when expected is matching with callback result", function () {
-        var expectedResult = 16; // 2^4
-        var expectedInput  = 4;
-
-        var calculation = function(input) {
-            return Math.pow(2, input);
-        };
-
-        var inputRangeFrom = 0;
-        var inputRangeTo   = 20;
-
-        expect(approximate(
-            calculation, expectedResult, inputRangeFrom, inputRangeTo
-        )).toEqual(expectedInput);
+        expect(approximate(power2, 16, 0, 10)).toEqual(4);
     });
 
     it("should find correct input value on upper edge", function () {
-        var expectedResult = 16; // 2^4
-        var expectedInput  = 4;
-
-        var calculation = function(input) {
-            return Math.pow(2, input);
-        };
-
-        var inputRangeFrom = 0;
-        var inputRangeTo   = 4;
-
-        expect(approximate(
-            calculation, expectedResult, inputRangeFrom, inputRangeTo
-        )).toEqual(expectedInput);
+        expect(approximate(power2, 16, 0, 4)).toEqual(4);
     });
 
     it("should find correct input value on lower edge", function () {
-        var expectedResult = 16; // 2^4
-        var expectedInput  = 4;
-
-        var calculation = function(input) {
-            return Math.pow(2, input);
-        };
-
-        var inputRangeFrom = 4;
-        var inputRangeTo   = 20;
-
-        expect(approximate(
-            calculation, expectedResult, inputRangeFrom, inputRangeTo
-        )).toEqual(expectedInput);
+        expect(approximate(power2, 16, 4, 10)).toEqual(4);
     });
 
     it("should return false when expected cannot be matched with callback result", function () {
-        var expectedResult = 17;
-
-        var calculation = function(input) {
-            return Math.pow(2, input);
-        };
-
-        var inputRangeFrom = 0;
-        var inputRangeTo   = 20;
-
-        expect(approximate(
-            calculation, expectedResult, inputRangeFrom, inputRangeTo
-        )).toEqual(false);
+        expect(approximate(power2, 17, 0, 10)).toEqual(false);
     });
 
     xit("should find correct input value when expected is outside initial range", function () {
