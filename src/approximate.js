8a69ae57ca2c7fa64c06ba778b1e1a60d12c5be2
janpapenbrock <j.papenbrock@hochzehn.com>
2016-12-20 15:10:21 +0100

    Implement simple approximation test case.

--- src/approximate.js
+++ src/approximate.js
@@ -1,3 +1,39 @@
-var approximate = function () {
+var approximate = function (calculation, expectedResult, inputRangeFrom, inputRangeTo) {
+    var currentInputRangeFrom = inputRangeFrom;
+    var currentInputRangeTo = inputRangeTo;
 
+    var currentInput;
+    var currentOutput;
+
+    function step() {
+        console.log("range:     " + currentInputRangeFrom + " ... " + currentInputRangeTo);
+
+        currentInput = Math.ceil(currentInputRangeFrom + (currentInputRangeTo - currentInputRangeFrom) / 2);
+
+        console.log("calc with: " + currentInput);
+
+        currentOutput = calculation(currentInput);
+
+        console.log("result: " + currentOutput + ", expecting " + expectedResult);
+
+        if (currentOutput == expectedResult) {
+            console.log("OK, cool, found the result.");
+            return currentInput;
+        }
+
+        if (currentOutput > expectedResult) {
+            console.log("Setting range upper to " + currentInput);
+            currentInputRangeTo = currentInput;
+        } else if (currentOutput < expectedResult) {
+            console.log("Setting range lower to " + currentInput);
+            currentInputRangeFrom = currentInput;
+        }
+    }
+
+    var result;
+    do {
+        result = step();
+    } while (!result);
+
+    return result;
 };
--- test/spec/approximateSpec.js
+++ test/spec/approximateSpec.js
@@ -1,6 +1,26 @@
 describe("approximate", function () {
 
-    it("should do nothing", function () {
+    it("should find correct input value when expected is matching with callback result", function () {
+        var expectedResult = 16; // 2^4
+        var expectedInput  = 4;
+
+        var calculation = function(input) {
+            return Math.pow(2, input);
+        };
+
+        var inputRangeFrom = 0;
+        var inputRangeTo   = 20;
+
+        expect(approximate(
+            calculation, expectedResult, inputRangeFrom, inputRangeTo
+        )).toEqual(expectedInput);
+    });
+
+    xit("should find closest input value when expected is not exactly matching with callback result", function () {
+        expect(approximate()).toEqual(undefined);
+    });
+
+    xit("should find correct input value when expected is outside initial range", function () {
         expect(approximate()).toEqual(undefined);
     });
 
